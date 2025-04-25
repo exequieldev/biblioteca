251e3c5c23a40598adae840c374285b647ccdc36
exequieldev <exequielandresdiaz@gmail.com>
2025-04-25 17:26:23 -0300

    Se crearon las migraciones para libro,prestamos y sancion

---
+++ database/migrations/2025_04_25_201617_create_prestamos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prestamos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('usuario_id')->constrained('users');
+            $table->foreignId('libro_id')->constrained('libros');
+            $table->date('fecha_prestamo');
+            $table->date('fecha_devolucion');
+            $table->boolean('devuelto')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prestamos');
+    }
+};
